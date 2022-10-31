cb288cf6f63158fb5ba165a58b4c8d05b934f788
evg-zlg <evg.zalogin@gmail.com>
2022-10-31 12:42:11 +0500

    feat: finish task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const fs = require("fs");
+const path = require("path");
+const dest = path.join(__dirname, "/", "files-copy");
+const src = path.join(__dirname, "/", "files");
+
+// create destination folder
+fs.mkdir(dest, {recursive : true}, (err) => {}); 
+
+fs.readdir(dest, function (err, files) {
+  // clear destination folder
+  files.forEach(file => {
+    fs.unlink(path.join(dest, file), err => {});
+  });
+  fs.readdir(src, function (err, files) {
+    // copy files
+    files.forEach(file => {
+      fs.copyFile(path.join(src, file), path.join(dest, file), err => {
+        if (err) {console.log("err = "+err)}
+      });
+    }); 
+  });
+});
 
